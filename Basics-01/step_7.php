031f4313d3c4c3f5d8adf8f7db03619447c1af3a
Yasmine Hannouf <yasmina.hannouf.2018@gmail.com>
2023-02-19 20:37:43 +0200

    PHP Control Statements -  If / else

---
+++ Basics-01/step_7.php
@@ -0,0 +1,8 @@
+<?php 
+
+$birth_year = 1997;
+$current_year = 2023;
+
+$difference = $current_year - $birth_year ;
+  echo $difference > 18 ?  "You can drive" : "You still a kid, go and play GTA"
+?>
