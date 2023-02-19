5c809f15e97341a6a13169e9ed4f89917d25d7ed
Yasmine Hannouf <yasmina.hannouf.2018@gmail.com>
2023-02-19 18:59:50 +0200

    Variables lvl 1

---
+++ Basics-01/step_3.php
@@ -0,0 +1,15 @@
+<?php
+$variable_name = 'Yasmine';
+$email= 'yasmina.hannouf.2018@gmail.com';
+
+
+echo  "My name is \"$variable_name\" and my email is \"$email.";
+
+$height=10;
+$width=20;
+
+$area=$height+$width;
+
+echo "the area is $area";
+
+?>
