9da82b60b223814b1e29b927dd544a3e006df09a
Yasmine Hannouf <yasmina.hannouf.2018@gmail.com>
2023-02-19 19:22:54 +0200

    Strings Concatenation

---
+++ Basics-01/step_4.php
@@ -0,0 +1,8 @@
+<?php 
+$name= 'yasmine';
+$postion= 'web developer';
+$github_url= "https://github.com/YasmineHannouf";
+
+echo  "My name is \"$name\" I'm \"$position\" please check my \"$github_url\".";
+
+?>
